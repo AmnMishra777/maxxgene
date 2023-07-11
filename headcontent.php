76f1c4ba10e36a12e19f85f221b01921fe01a069
Aman Mishra <plot168@gmail.com>
2023-07-11 13:56:32 +0530

    eleventh commit

--- headcontent.php
+++ headcontent.php
@@ -66,7 +66,7 @@ function topbanner()
 function nav($key)
 {
 	$menu ="<nav class='navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0'>
-            <a href='' class='navbar-brand p-0'>
+            <a href='index.html' class='navbar-brand p-0'>
                 <h1 class='text-primary m-0'><i class='fa fa-map-marker-alt me-3'></i>MAXXGENE</h1>
                 <!-- <img src='img/logo.png' alt='Logo'> -->
             </a>
